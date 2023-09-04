511e8279d65e8a9730c87b3ab23311af2ac3e383
Iain Taylor <iain.taylor.94@gmail.com>
2023-09-04 14:24:06 +0000

    copied program

--- .ccls-cache/@home@runner@Ch11Q1P6-using-linked-lists/main.c
+++ .ccls-cache/@home@runner@Ch11Q1P6-using-linked-lists/main.c
@@ -1,6 +1,29 @@
+// Function to use linked lists
+
 #include <stdio.h>
 
+// Define Structures
+struct entry {
+int value;
+struct entry *next;
+};
+
 int main(void) {
-  printf("Hello World\n");
+
+  struct entry n1, n2, n3;
+  int i;
+
+  n1.value = 100;
+  n2.value = 200;
+  n3.value = 300;
+
+  n1.next = &n2;
+  n2.next = &n3;
+
+  i = n1.next->value;
+  printf ("%d  ", i);
+
+  printf ("%d\n", n2.next->value);
+  
   return 0;
 }
